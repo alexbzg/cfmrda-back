72aaeb55b5a212b913965a2c7731bee2ff10455c
root <alexbzg@gmail.com>
2018-11-19 18:04:17 +0300

    -

--- cfmrda/db.py
+++ cfmrda/db.py
@@ -66,6 +66,7 @@ class DBConn:
     def connect(self):
         try:
             self.pool = yield from aiopg.create_pool(self.dsn, \
+
                     on_connect=init_connection)
             logging.debug('db connections pool created')
         except Exception:
