e138bd4aa7cb2548dcea83801003a0027c4b716f
root <alexbzg@gmail.com>
2019-03-21 14:23:39 +0300

    -

---
+++ cfmrda/cluster_filter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+#coding=utf-8
+"""functions for exporting stats data from db to frontend json files"""
+import asyncio
+import logging
+
+from common import site_conf, start_logging
+from db import DBConn
+from json_utils import load_json, save_json
+
+@asyncio.coroutine
+def perform(conf):
+    """cluster filtering"""
+    logging.debug('cluster filtering')
+
+    _db = DBConn(conf.items('cluster_db'))
+    yield from _db.connect()
+
+    _dx = load_json(conf.get('files', 'cluster'))
+    if not _dx:
+        logging.error('Cluster data not found')
+        return
+
+    dxped = yield from _db.execute("""
+                select json_object_agg(callsign, 
+                    json_build_object('desc', descr, 'link', link)) as data
+                from dxpedition 
+                where (dt_begin < now() or dt_begin is null) 
+                    and (dt_end > now() or dt_end is null)""", False)
+    rda_dx_fname = conf.get('web', 'root') + '/json/dx.json'
+    rda_dx = load_json(rda_dx_fname)
+    prev = None
+    if rda_dx:
+        prev = rda_dx[0]
+    else:
+        rda_dx = []
+    idx = 0
+    for item in reversed(_dx):
+        if prev and item['ts'] < prev['ts']:
+            break
+        if 'RDA' in item['awards'] or item['cs'] in dxped:
+            if item['cs'] in dxped:
+                item['dxped'] = dxped[item['cs']]
+            rda_dx.insert(idx, item)
+            idx += 1
+    if len(rda_dx) > 20:
+        rda_dx = rda_dx[:20]
+    save_json(rda_dx, rda_dx_fname)
+
+def main():
+    """call from shell"""
+    conf = site_conf()
+    start_logging('cluster')
+
+    asyncio.get_event_loop().run_until_complete(perform(conf))
+
+if __name__ == "__main__":
+    main()
+
