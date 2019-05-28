d3d75e19517cdd58ec9057cf26dcaa05b94c69c3
root <alexbzg@gmail.com>
2019-05-28 16:31:54 +0300

    -

--- cfmrda.sql
+++ cfmrda.sql
@@ -391,22 +391,19 @@ CREATE FUNCTION tf_callsigns_rda_bi() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 begin
+  if exists (select from callsigns_rda 
+    where callsign = new.callsign and source = new.source and 
+      rda = new.rda and
+      (dt_start is null or dt_start <= new.dt_start) and
+      (dt_stop is null or dt_stop >= new.dt_stop))
+  then
+    return null;
+  end if;      
   if (new.source = 'QRZ.ru')
   then
-    if exists (select from callsigns_rda where source = 'QRZ.ru' and callsign = new.callsign)
-    then
-      update callsigns_rda 
-      set rda = new.rda, ts = now() 
-      where callsign = new.callsign and source = 'QRZ.ru';
-      return null;
-    end if;
-  else
-    if exists (select from callsigns_rda where source != 'QRZ.ru' and callsign = new.callsign 
-      and (dt_start <= new.dt_stop or dt_start is null or new.dt_stop is null)
-      and (dt_stop >= new.dt_start or dt_stop is null or new.dt_start is null))
-    then
-      raise 'Конфликт в истории RDA';
-    end if;
+    update callsigns_rda 
+      set dt_stop = now() 
+      where callsign = new.callsign and source = 'QRZ.ru' and dt_stop is null;
   end if;
   return new;
 end$$;
@@ -421,7 +418,7 @@ ALTER FUNCTION public.tf_callsigns_rda_bi() OWNER TO postgres;
 CREATE FUNCTION tf_cfm_qsl_qso_bi() RETURNS trigger
     LANGUAGE plpgsql
     AS $$begin
-  select from check_qso(new.callsign, new.station_callsign, new.rda, new.band, new.mode, new.tstamp);
+  perform check_qso(new.callsign, new.station_callsign, new.rda, new.band, new.mode, new.tstamp);
 /*  if exists (select 1 from cfm_qsl_qso
 	where callsign = new.callsign and rda = new.rda
 	and station_callsign = new.station_callsign
@@ -462,7 +459,7 @@ ALTER FUNCTION public.tf_cfm_qsl_qso_bu() OWNER TO postgres;
 CREATE FUNCTION tf_cfm_request_qso_bi() RETURNS trigger
     LANGUAGE plpgsql
     AS $$begin
-  select from check_qso(new.callsign, new.station_callsign, new.rda, new.band, new.mode, new.tstamp);
+  perform check_qso(new.callsign, new.station_callsign, new.rda, new.band, new.mode, new.tstamp);
   if exists (select 1 from cfm_request_qso
 	where callsign = new.callsign and rda = new.rda
 	and station_callsign = new.station_callsign
