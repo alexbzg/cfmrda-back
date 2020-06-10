127b97c2e503bbcc58496544d55992fa98ffc9cd
root <alexbzg@gmail.com>
2020-06-10 14:54:36 +0300

    truncating super long string fields before jsonschema validation

--- cfmrda/json_utils.py
+++ cfmrda/json_utils.py
@@ -12,6 +12,7 @@ import jsonschema
 
 def json_encode_extra(obj):
     """encoder for json.dump(s) with extra types support"""
+#    return str(type(obj))
     if isinstance(obj, decimal.Decimal):
         return float(obj)
     elif isinstance(obj, datetime):
@@ -37,6 +38,23 @@ def save_json(data, path_json):
     with open(path_json, 'w') as file:
         json.dump(data, file, ensure_ascii=False)
 
+def deep_copy_trunc(src, size_limit=1024):
+    rslt = None
+    if isinstance(src, dict):
+        rslt = {}
+        for key, val in src.items():
+            rslt[key] = deep_copy_trunc(val, size_limit)
+    elif isinstance(src, list):
+        rslt = []
+        for item in src:
+            rslt.append(deep_copy_trunc(item, size_limit))
+    elif isinstance(src, str):
+        rslt = src if size_limit > 0 and len(src) < size_limit\
+                else src[:size_limit] + '_etc'
+    else:
+        rslt = src
+    return rslt
+        
 class JSONvalidator:
 
     def __init__(self, schemas):
@@ -44,10 +62,9 @@ class JSONvalidator:
 
     def validate(self, schema, data):
         try:
-            jsonschema.validate(data, self._schemas[schema])
+            jsonschema.validate(deep_copy_trunc(data), self._schemas[schema])
             return True
         except jsonschema.exceptions.ValidationError as exc:
             logging.error('Error validating json data. Schema: ' + schema)
-            logging.error(data)
-            logging.error(exc.message)
+            logging.exception(exc)
             return False
