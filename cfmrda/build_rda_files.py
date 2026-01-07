7b965a6f74034b82c67ae45a3cab79162e4e1170
root <root@194-67-78-213.cloudvps.regruhosting.ru>
2026-01-07 08:25:14 +0300

    sort rda before building files for front

--- cfmrda/build_rda_files.py
+++ cfmrda/build_rda_files.py
@@ -8,7 +8,7 @@ import requests
 
 from json_utils import save_json
 
-rda_rus_rsp = requests.get('https://rdaward.ru/share/RDA_list_2025.csv')
+rda_rus_rsp = requests.get('https://rdaward.ru/share/RDA_list_2026.csv')
 rda_rus_rsp.encoding = 'cp1251'
 lines = rda_rus_rsp.text.split('\r\n')[1:]
 
@@ -16,23 +16,29 @@ rda_titles = {}
 rda_list_short = []
 rda_list_full = []
 
+rdas = []
 for line in lines:
     if line.count(';') == 2:
         value, _, title = line.split(';')
-        rda_titles[value] = title
+        rdas.append((value, title))
 
-        rda_list_full.append(value)
+rdas.sort()
 
-        group = value[:2]
-        if not rda_list_short or rda_list_short[-1]['group'] != group:
-            rda_list_short.append({'group': group, 'last': 0})
-        int_val = int(value[3:])
-        if rda_list_short[-1]['last'] + 1 < int_val:
-            rda_list_short[-1].setdefault('skip', [])
-            rda_list_short[-1]['skip'].extend(
-                list(range(rda_list_short[-1]['last'] + 1, int_val))
-                )
-        rda_list_short[-1]['last'] = int_val
+for value, title in rdas:
+    rda_titles[value] = title
+
+    rda_list_full.append(value)
+
+    group = value[:2]
+    if not rda_list_short or rda_list_short[-1]['group'] != group:
+        rda_list_short.append({'group': group, 'last': 0})
+    int_val = int(value[3:])
+    if rda_list_short[-1]['last'] + 1 < int_val:
+        rda_list_short[-1].setdefault('skip', [])
+        rda_list_short[-1]['skip'].extend(
+            list(range(rda_list_short[-1]['last'] + 1, int_val))
+            )
+    rda_list_short[-1]['last'] = int_val
 
 
 save_json(rda_titles, '/var/www/cfmrda-dev/public/json/rda_titles.json')
